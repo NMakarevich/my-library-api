724f418623d2056a1f1c134fd0d1c82a160267cf
NMakarevich <nikoladze529@mail.ru>
2023-06-02 15:35:37 +0300

    fix: add public decorator

--- src/resources/uploads/uploads.controller.ts
+++ src/resources/uploads/uploads.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { UploadsService } from './uploads.service';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('uploads')
 export class UploadsController {
   constructor(private readonly uploadsService: UploadsService) {}
 
+  @Public()
   @Get('**')
   getPhoto(@Req() req: Request) {
     return this.uploadsService.getPhoto(req.url);
